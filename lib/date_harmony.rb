33424e5c1610dc65b5c02846cdd19c52136aa615
Frank Rietta <frank@rietta.com>
2012-11-12 10:59:32 -0500

    Moving the regex and parse strings into an array for easier maintenance.
    Signed-off-by: Frank Rietta <frank@rietta.com>

--- lib/date_harmony.rb
+++ lib/date_harmony.rb
@@ -1,90 +1,133 @@
 class DateHarmony
   VERSION = '1.0.0'
   
-    def parse_aggresively(dv)
-    dayInfo = nil
+  DATE_TIME_PATTERNS = [
+    ['0000-00-00 00:00:00', nil, "MySQL NULL date/time not parseable"],
+    [/\A[0-9]{4}\-[0-9]{2}\-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\Z/, "%Y-%m-%d %H:%M:%S", "MySQL Timestamp 0000-00-00 00:00:00"],
+    [/\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2} [0-9]{1,2}:[0-9]{1,2}[| \t]{0,2}(am|pm|AM|PM)\Z/, "%m/%d/%y %H:%M %p", "Two digit American 3/25/04 1:13 AM"],
+    [/\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4} [0-9]{1,2}:[0-9]{1,2}[| \t]{0,2}(am|pm|AM|PM)\Z/, "%m/%d/%Y %H:%M %p", "Four digit American 3/25/2004 13:06 OR 3/25/2004 13:06"],
+    [/\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2} [0-9]{1,2}:[0-9]{1,2}\Z/, "%m/%d/%y %H:%M", "Two digit American 3/25/04 13:06"],
+    [/\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4} [0-9]{1,2}:[0-9]{1,2}\Z/, "%m/%d/%Y %H:%M", "Four digit American 3/25/2004 13:06"],
+    [/\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}\Z/, "%m/%d/%Y", "US Customary Format with Four Digit Year"],
+    [/\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2}\Z/, "%m/%d/%y", "US Customary Format with 2 Digit Year"],
+    [/\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4} [0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}\Z/, "%m/%d/%Y %H:%M:%S", "US 4 digit year, hours, minutes, and seconds"],
+    [/\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2} [0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}\Z/, "%m/%d/%y %H:%M:%S", "US 2 digit year, hours, minutes, and seconds"],
+    [/\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4} [0-9]{1,2}:[0-9]{1,2}\Z/, "%m/%d/%Y %H:%M", "US 4 digit year, hours, and minutes"],
+    [/\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2} [0-9]{1,2}:[0-9]{1,2}\Z/, "%m/%d/%y %H:%M", "US 2 digit year, hours, and minutes"],
+    [],
+    [],
+    [],
+  ]
+  
+  DATE_PATTERNS = [
+      [/\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}\Z/, "%m/%d/%Y", "US customary month/day/year with 4 digit year"],
+      [/\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2}\Z/, "%m/%d/%y", "US customary month/day/year with 2 digit year"],
+      [/\A[0-9]{1,2}-[a-zA-Z]{3}-[0-9]{2}\Z/, "%d-%b-%y", "Like 3-Nov-65"],
+      [/\A[0-9]{1,2}-[a-zA-Z]{3}-[0-9]{4}\Z/, "%d-%b-%Y", "Like 3-Nov-1965"],
+      [/\A[0-9]{1,2}-[a-zA-Z]{3,9}-[0-9]{2}\Z/, "%d-%B-%y", "Like 3-November-65"],
+      [/\A[0-9]{1,2}-[a-zA-Z]{3,9}-[0-9]{4}\Z/, "%d-%B-%Y", "Like 3-November-1965"],
+      [],
+      [],
+    ]
+  
+  
+  def parse_aggresively(dv)
+    day_info = nil
     begin
       
-      if ('0000-00-00 00:00:00' == dv)
+      #if ('0000-00-00 00:00:00' == dv)
         # MySQL NULL date/time not parseable
-        dayInfo = nil
-      elsif (dv =~ /\A[0-9]{4}\-[0-9]{2}\-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\Z/) != nil
+      #  day_info = nil
+      #elsif (dv =~ /\A[0-9]{4}\-[0-9]{2}\-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}\Z/) != nil
           # MySQL Timestamp 0000-00-00 00:00:00
-          dayInfo = DateTime.strptime(dv.to_s.upcase, "%Y-%m-%d %H:%M:%S")
-      elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2} [0-9]{1,2}:[0-9]{1,2}[| \t]{0,2}(am|pm|AM|PM)\Z/) != nil 
+      #    day_info = DateTime.strptime(dv.to_s.upcase, "%Y-%m-%d %H:%M:%S")
+      #elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2} [0-9]{1,2}:[0-9]{1,2}[| \t]{0,2}(am|pm|AM|PM)\Z/) != nil 
           # Two digit American 3/25/04 1:13 AM
-          dayInfo = DateTime.strptime(dv.to_s.upcase, "%m/%d/%y %H:%M %p")
-      elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4} [0-9]{1,2}:[0-9]{1,2}[| \t]{0,2}(am|pm|AM|PM)\Z/) != nil 
+      #    day_info = DateTime.strptime(dv.to_s.upcase, "%m/%d/%y %H:%M %p")
+      #elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4} [0-9]{1,2}:[0-9]{1,2}[| \t]{0,2}(am|pm|AM|PM)\Z/) != nil 
           # Four digit American 3/25/2004 13:06 OR 3/25/2004 13:06
-          dayInfo = DateTime.strptime(dv.to_s.upcase, "%m/%d/%Y %H:%M %p")
-      elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2} [0-9]{1,2}:[0-9]{1,2}\Z/) != nil  
+      #    day_info = DateTime.strptime(dv.to_s.upcase, "%m/%d/%Y %H:%M %p")
+      #elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2} [0-9]{1,2}:[0-9]{1,2}\Z/) != nil  
           # Two digit American 3/25/04 13:06
-          dayInfo = DateTime.strptime(dv, "%m/%d/%y %H:%M")
-      elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4} [0-9]{1,2}:[0-9]{1,2}\Z/) != nil
+      #    day_info = DateTime.strptime(dv, "%m/%d/%y %H:%M")
+      #elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4} [0-9]{1,2}:[0-9]{1,2}\Z/) != nil
           # Four digit American 3/25/2004 13:06 
-          dayInfo = DateTime.strptime(dv, "%m/%d/%Y %H:%M")
-      elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}\Z/) != nil
+      #    day_info = DateTime.strptime(dv, "%m/%d/%Y %H:%M")
+      #elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}\Z/) != nil
         # US Customary Format with Four Digit Year
-        dayInfo = Date.strptime(dv, "%m/%d/%Y")
-      elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2}\Z/) != nil
+    #    day_info = Date.strptime(dv, "%m/%d/%Y")
+      #elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2}\Z/) != nil
         # US Customary Format with 2 Digit Year
-        dayInfo = Date.strptime(dv, "%m/%d/%y")
-      elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4} [0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}\Z/) != nil
+      #  day_info = Date.strptime(dv, "%m/%d/%y")
+      #elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4} [0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}\Z/) != nil
         # US Customary Format with Four Digit Year
-        dayInfo = DateTime.strptime(dv, "%m/%d/%Y %H:%M:%S")
-      elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2} [0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}\Z/) != nil
+      #  day_info = DateTime.strptime(dv, "%m/%d/%Y %H:%M:%S")
+      #elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2} [0-9]{1,2}:[0-9]{1,2}:[0-9]{1,2}\Z/) != nil
         # US Customary Format with 2 Digit Year
-        dayInfo = DateTime.strptime(dv, "%m/%d/%y %H:%M:%S")
-      elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4} [0-9]{1,2}:[0-9]{1,2}\Z/) != nil
+      #  day_info = DateTime.strptime(dv, "%m/%d/%y %H:%M:%S")
+      #elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4} [0-9]{1,2}:[0-9]{1,2}\Z/) != nil
           # US Customary Format with Four Digit Year
-          dayInfo = DateTime.strptime(dv, "%m/%d/%Y %H:%M")
-      elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2} [0-9]{1,2}:[0-9]{1,2}\Z/) != nil
+      #    day_info = DateTime.strptime(dv, "%m/%d/%Y %H:%M")
+      #elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2} [0-9]{1,2}:[0-9]{1,2}\Z/) != nil
           # US Customary Format with 2 Digit Year
-          dayInfo = DateTime.strptime(dv, "%m/%d/%y %H:%M")
-      else
-        dayInfo = DateTime.parse(dv)
-      end
+      #    day_info = DateTime.strptime(dv, "%m/%d/%y %H:%M")
+      #else
+      #  day_info = DateTime.parse(dv)
+      #end
    
     rescue
-      dayInfo = nil
+      day_info = nil
     end # 
   
-    return dayInfo
+    return day_info
   end # parse_date_time_aggresively
   
   
   def parse_date(dv)
      begin
       
-        if (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}\Z/) != nil
+        #if (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}\Z/) != nil
           # US Customary Format with Four Digit Year
-          dayInfo = Date.strptime(dv, "%m/%d/%Y")
-        elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2}\Z/) != nil
+        #  day_info = Date.strptime(dv, "%m/%d/%Y")
+        #elsif (dv =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2}\Z/) != nil
           # US Customary Format with 2 Digit Year
-          dayInfo = Date.strptime(dv, "%m/%d/%y")
-        elsif (dv =~ /\A[0-9]{1,2}-[a-zA-Z]{3}-[0-9]{2}\Z/) != nil
+        #  day_info = Date.strptime(dv, "%m/%d/%y")
+        #elsif (dv =~ /\A[0-9]{1,2}-[a-zA-Z]{3}-[0-9]{2}\Z/) != nil
           # 3-Nov-65
-          dayInfo = Date.strptime(dv, "%d-%b-%y")
-        elsif (dv =~ /\A[0-9]{1,2}-[a-zA-Z]{3}-[0-9]{4}\Z/) != nil
+        #  day_info = Date.strptime(dv, "%d-%b-%y")
+        #elsif (dv =~ /\A[0-9]{1,2}-[a-zA-Z]{3}-[0-9]{4}\Z/) != nil
           # 3-Nov-1965
-          dayInfo = Date.strptime(dv, "%d-%b-%Y")
-        elsif (dv =~ /\A[0-9]{1,2}-[a-zA-Z]{3,9}-[0-9]{4}\Z/) != nil
+        #  day_info = Date.strptime(dv, "%d-%b-%Y")
+        #elsif (dv =~ /\A[0-9]{1,2}-[a-zA-Z]{3,9}-[0-9]{4}\Z/) != nil
+          # 3-November-1965
+        #  day_info = Date.strptime(dv, "%d-%B-%Y")
+        #elsif (dv =~ /\A[0-9]{1,2}-[a-zA-Z]{3,9}-[0-9]{2}\Z/) != nil
           # 3-November-65
-          dayInfo = Date.strptime(dv, "%d-%B-%Y")
-        elsif (dv =~ /\A[0-9]{1,2}-[a-zA-Z]{3,9}-[0-9]{2}\Z/) != nil
-          # 3-November-65
-          dayInfo = Date.strptime(dv, "%d-%B-%y")
-        else
-          dayInfo = Date.parse(dv)
+        #  day_info = Date.strptime(dv, "%d-%B-%y")
+        #else
+        #  day_info = Date.parse(dv)
+        #end
+        
+        match_found = false
+        DATE_PATTERNS.each do |pattern_item|
+          if (dv =~ pattern_item[0])
+            match_found = true
+            day_info = Date.strptime(pattern_item[1])
+          end
         end
+        
+        # If no match was found, see if the date object can parse this itself.
+        # This will probably fail.
+        unless match_found
+          day_info = Date.parse(dv)
+        end
+        
       rescue
-        dayInfo = nil
-          #puts "\n\n-- ERROR: COULD NOT PARSE DATE '#{dv}'\n\n"
-          #do something if invalid
+        day_info = nil
       end
       
-      if nil != dayInfo
-        dayInfo.strftime("%Y-%m-%d")
+      if nil != day_info
+        day_info.strftime("%Y-%m-%d")
       else
         dv
       end
@@ -123,15 +166,15 @@ class DateHarmony
 
             if (s =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{4}\Z/) != nil
               # US Customary Format with Four Digit Year
-              dayInfo = Date.strptime(s, "%m/%d/%Y")
+              day_info = Date.strptime(s, "%m/%d/%Y")
             elsif (s =~ /\A[0-9]{1,2}\/[0-9]{1,2}\/[0-9]{2}\Z/) != nil
               # US Customary Format with 2 Digit Year
-              dayInfo = Date.strptime(s, "%m/%d/%y")
+              day_info = Date.strptime(s, "%m/%d/%y")
             else
-              dayInfo = Date.parse(s)
+              day_info = Date.parse(s)
             end
           rescue
-            dayInfo = nil
+            day_info = nil
             is_date = false
           end
     end
